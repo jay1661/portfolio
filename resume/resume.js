f3bbb110134204bbc2acf841007f22afd1168e5b
jay1661 <106527903+jay1661@users.noreply.github.com>
2023-12-16 18:56:05 +0530

    Update resume.js

--- resume/resume.js
+++ resume/resume.js
@@ -1,14 +1,1 @@
-//  // JavaScript to handle the download
-//  document.getElementById('downloadButton').addEventListener('click', function() {
-//     // Replace 'path/to/your/resume.pdf' with the actual path to your PDF file
-//     var resumePath = 'portfolio\resume\Mistry-Jay-Maheshkumar-FlowCV-Resume-20231204.pdf';
-    
-//     // Create a link element, set the download attribute and simulate a click
-//     var a = document.createElement('a');
-//     a.href = resumePath;
-//     a.download = 'portfolio\resume\Mistry-Jay-Maheshkumar-FlowCV-Resume-20231204.pdf';  // Set the desired file name with extension
-//     a.style.display = 'none';
-//     document.body.appendChild(a);
-//     a.click();
-//     document.body.removeChild(a);
-// });
+
